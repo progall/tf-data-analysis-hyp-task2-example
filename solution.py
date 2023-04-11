320718d0679084cdab45515bc4f56d7129e71598
progall <progall@users.noreply.github.com>
2023-04-11 16:04:36 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+from scipy.stats import pareto, cauchy, norm, ttest_ind, ks_2samp, mannwhitneyu, permutation_test
 
 chat_id = 322172960 # Ваш chat ID, не меняйте название переменной
 
@@ -8,4 +8,4 @@ def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return ks_2samp(x, y, alternative="two-sided").pvalue < 0.01 # Ваш ответ, True или False
